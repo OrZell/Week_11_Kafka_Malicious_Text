106da0d978dce408de403ff7f5e6f8834e92f8a9
Or Zellinger <or.zellinger@gmail.com>
2025-08-28 16:18:53 +0300

    first version works

--- Persister/mainPersister.py
+++ Persister/mainPersister.py
@@ -3,10 +3,5 @@ from maneger_persister import ManegerPersister
 
 app = ManegerPersister()
 
-def main():
-    while True:
-        app.run_persister_service()
-
-
 if __name__ == '__main__':
-    main()
+    app.run_persister_service()
